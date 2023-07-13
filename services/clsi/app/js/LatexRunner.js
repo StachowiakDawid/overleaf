2b0861a413e95d485a3191e4428f5a07e5788220
StachowiakDawid <dawidos234@gmail.com>
2023-07-13 21:05:49 +0200

    Add Tectonic to compilers

--- services/clsi/app/js/LatexRunner.js
+++ services/clsi/app/js/LatexRunner.js
@@ -95,6 +95,12 @@ function runLatex(projectId, options, callback) {
           }
         }
       }
+      if (compiler === 'tectonic') {
+        fs.rename(Path.join(directory, mainFile.replace('tex', 'pdf')), Path.join(directory, 'output.pdf'), function (err) {
+          if (err) logger.debug(err)
+          if (err) throw err
+        })
+      }
       // record output files
       _writeLogOutput(projectId, directory, output, () => {
         callback(error, output)
@@ -143,6 +149,7 @@ function killLatex(projectId, callback) {
 }
 
 function _buildLatexCommand(mainFile, opts = {}) {
+  if (opts.compiler === 'tectonic') return ['tectonic', '--keep-logs', Path.join('$COMPILE_DIR', mainFile)] 
   const command = []
 
   if (Settings.clsi?.strace) {
